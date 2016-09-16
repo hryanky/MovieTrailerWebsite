0c8b4ecd211be10637c77db72492036f9994be4c
hryanky <hryanky@gmail.com>
2016-09-16 12:17:26 -0700

    Added comments and indentation.

--- media.py
+++ media.py
@@ -1,13 +1,21 @@
 import webbrowser
 import fresh_tomatoes
 
+
 class Movie():
-    VALID_RATINGS = ["P","PG"] # class variables
-    def __init__(self,movie_title,movie_storyline,poster_image,youtube_url):
+    """ This is class documentation string..
+    class instantiation calls init() and contructs the object.
+    """
+    VALID_RATINGS = ["P", "PG"]  # class variables
+
+    def __init__(self, movie_title, movie_storyline, poster_img, youtube_url):
+        """This is a class constructor.
+        Assigns the data associated to the instance variables"""
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image_url = poster_image
+        self.poster_image_url = poster_img
         self.trailer_youtube_url = youtube_url
-
+    """ show_trailer() docstring..
+    This is an instance method associated to the instance created."""
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
